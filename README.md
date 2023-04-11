47bcadbd5b366cb5929043bef0beef7620f4d06f
Veronika Bendova <126337758+beveroni@users.noreply.github.com>
2023-04-11 18:37:26 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Palindromy
+
+Postupujte podle zadání cvičení [Anagramy](https://kodim.cz/kurzy/daweb/js1/cykly/cv-foreach#cvlekce%3Epalindromy).
---
+++ app.js
@@ -0,0 +1,8 @@
+console.log('Funguju!')
+
+const textPozpatku = (text) => {
+	return text.split('').reverse().join('')
+}
+
+// textPozpatku('Ahoj světe') // 'etěvs johA'
+// textPozpatku('Kajak') // 'kajaK'
