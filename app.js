39c86a218aa4dd58018aa69529e0f7db8dd47704
Veronika Bendova <svechovaver@gmail.com>
2023-07-28 22:40:16 +0200

    Task done

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
-console.log('Funguju!')
+console.log('Funguju!');
 
 const textPozpatku = (text) => {
-	return text.split('').reverse().join('')
-}
+  return text.split('').reverse().join('');
+};
+
+const selectLi = document.querySelectorAll('li');
+selectLi.forEach((polozka) => {
+  const text = polozka.textContent.toLocaleLowerCase().replaceAll(' ', '');
+  if (text === textPozpatku(text)) {
+    polozka.classList.add('palindrom');
+  }
+});
 
 // textPozpatku('Ahoj světe') // 'etěvs johA'
 // textPozpatku('Kajak') // 'kajaK'
+
+// Upravte soubor ověřil, jestli se v jejich textovém obsahu nachází palindrom. Pokud ano, přidejte položce třídu palindrom (li.classLiat(".palindrom")), která má již přepřipravené CSS pro zvýraznění.
+
+// porovnat selectLi a textPozpatku
+//
